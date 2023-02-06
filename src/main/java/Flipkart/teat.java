4adefd837a4bf8d90b23060822b73bbe72fab43f
Dhana <dhanasailakshmi@colanonline.com>
2023-02-06 12:29:58 +0530

    first commit

---
+++ src/main/java/Flipkart/teat.java
@@ -0,0 +1,17 @@
+package Flipkart;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class teat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+WebDriverManager.chromedriver().setup();
+WebDriver driver=new ChromeDriver();
+
+	}
+
+}
